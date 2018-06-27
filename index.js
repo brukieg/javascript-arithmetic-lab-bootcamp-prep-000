e1c68eb952b32470467319e1abc38da3f3a32c23
Brukie G <brukieg@gmail.com>
2018-06-27 23:06:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 function add(a, b) {
-  return (a + b) 
+  return (a + b); 
 }
 function subtract(a,b) {
-  return (a - b)
+  return (a - b);
 }
 function multiply(a,b) {
-  return (a * b)
+  return (a * b);
 }
 function divide(a,b) {
-  return (a / b)
+  return (a / b);
 }
 var n = 5
+
 function inc(n) {
 n +=3;
 console.log(n);
